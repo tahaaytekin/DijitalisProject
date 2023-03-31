daa659551a6a32cf9da798a2c46af8f567bce3f7
MuhammetTahaAytekin <106655962+tahaaytekin@users.noreply.github.com>
2023-03-31 17:19:00 +0300

    Düzenleme

--- Assets/Scripts/Raf.cs
+++ Assets/Scripts/Raf.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using TMPro;
 using System.ComponentModel;
 
+public enum AddressType { A1, A2, A3, B1, B2, B3, C1, C2, C3, D1, D2, D3 }
 public enum MassType { Heavy, Light }
 public enum ContentType { drink, food }
 public class Raf : MonoBehaviour
 {
+    public TextMeshProUGUI addresTypeTxt;
+    public AddressType addressType;
+
     public List<MeshRenderer> meshRenderers;
 
     public int remainingTime;
@@ -22,13 +26,11 @@ public class Raf : MonoBehaviour
     private void Start()
     {
         Describing();
-    }
-    private void OnEnable()
-    {
         EventManager.Instance.OnContentType += ContentType;
         EventManager.Instance.OnMassType += MassType;
         EventManager.Instance.OnRemainingType += RemainingType;
     }
+
     private void OnDisable()
     {
         EventManager.Instance.OnContentType -= ContentType;
@@ -42,10 +44,12 @@ public class Raf : MonoBehaviour
         remainingTimeTxt.text = remainingTime.ToString();
 
         massTxt.text = massType.ToString();
+
+        addresTypeTxt.text = addressType.ToString();
     }
     public void ContentType()
     {
-        if (contentType==global::ContentType.drink)
+        if (contentType == global::ContentType.drink)
         {
             ColoringYellow();
         }
@@ -56,7 +60,7 @@ public class Raf : MonoBehaviour
     }
     public void MassType()
     {
-        if (massType==global::MassType.Heavy)
+        if (massType == global::MassType.Heavy)
         {
             ColoringRed();
         }
@@ -85,10 +89,10 @@ public class Raf : MonoBehaviour
     }
     public void ColoringGreen()
     {
-            foreach (var item in meshRenderers)
-            {
-                item.materials[0].color = Color.green;
-            }
+        foreach (var item in meshRenderers)
+        {
+            item.materials[0].color = Color.green;
+        }
     }
     public void ColoringBlue()
     {
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,15 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject choosePanel, uiButton;
+    Button btn;
+    Raf currentRaf;
+    public TextMeshProUGUI currentRemainingTimeTxt, currentMassTxt, currentContentType;
+    public GameObject choosePanel, uiButton, adreessContentDataPanel;
     public void OpenPanel()
     {
         uiButton.SetActive(false);
         choosePanel.SetActive(true);
         choosePanel.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.Linear);
+    }
+    public void OpenAddressPanel()
+    {
+        currentRaf = null;
+        GameObject go = EventSystem.current.currentSelectedGameObject;
+        currentRaf = go.GetComponentInParent<Raf>();
+
+        currentRemainingTimeTxt.text = "Remaining Time : " + currentRaf.remainingTime.ToString();
+
+        currentMassTxt.text = "Mass Type : " + currentRaf.massType.ToString();
+
+        currentContentType.text = "Content Type : " + currentRaf.contentType.ToString();
+
+        uiButton.SetActive(false);
+        choosePanel.SetActive(false);
+        adreessContentDataPanel.SetActive(true);
+        adreessContentDataPanel.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.Linear);
+    }
+    public void CloseAddressPanel()
+    {
+        adreessContentDataPanel.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            adreessContentDataPanel.SetActive(false);
+            uiButton.SetActive(true);
+        });
     }
     public void RemainingType()
     {
@@ -26,7 +58,7 @@ public class UIManager : MonoBehaviour
     }
     public void ExitButtonOnClick()
     {
-        choosePanel.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.Linear).OnComplete(() => 
+        choosePanel.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.Linear).OnComplete(() =>
         {
             choosePanel.SetActive(false);
             uiButton.SetActive(true);
